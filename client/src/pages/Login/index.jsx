0da67c0dc564930e4eb63ba75f51540f0766333a
WendellReis <wendellreismatias@gmail.com>
2026-04-10 09:40:53 -0300

    Tela de Login

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -2,22 +2,24 @@ import './styles.css'
 import logo from '../../assets/logo.png'
 
 function Login() {
-  return <div>
-    <img src={logo} alt="Logo"/>
-    <h1>
+  return <div className="login-container">
+    <img src={logo} alt="Logo" className="login-logo"/>
+    <h1 className="login-title">
         Mini-Splunk
     </h1>
-    <form>
-        <text>Login</text>
-        <text>Entre com as suas credencias para acessar o console</text>
+    <form className="login-form">
+        <p className="login-form-title">Autenticação</p>
+        <p className="login-form-subtitle">Entre com as suas credencias para acessar</p>
 
-        <text>EMAIL</text>
-        <input type="text" placeholder="seuemail@email.com"></input>
+        <p className="login-form-label">EMAIL</p>
+        <input className="login-input" type="text" placeholder="seuemail@email.com"></input>
 
-        <text>SENHA</text>
-        <input type="password" placeholder="********"></input>
+        <p className="login-form-label">SENHA</p>
+        <input className="login-input" type="password" placeholder="••••••••••"></input>
 
-        <button type="submit">Entrar</button>
+        <div>
+          <button className="login-button" type="submit">Login</button>
+        </div>
     </form>
   </div>
 }
